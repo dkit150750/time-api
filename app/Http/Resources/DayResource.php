66438a45c1e1657a58de20c30cad2470ebf1b5ae
dkit150750 <dkit150750@gmail.com>
2021-09-20 00:08:29 +0300

    rename

--- app/Http/Resources/DayResource.php
+++ app/Http/Resources/DayResource.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class DayResource extends JsonResource
+class DateResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
