9ab108835d29095e246a172b869364709c1daf70
dkit150750 <dkit150750@gmail.com>
2021-09-24 00:25:03 +0300

    fix

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -9,6 +9,7 @@ use App\Models\Change;
 use App\Models\Day;
 use App\Models\Group;
 use App\Models\Lesson;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
@@ -23,26 +24,26 @@ class GroupController extends Controller
     {
         $group = Group::select(['id', 'name', 'course', 'slug'])
             ->where('slug', $slug)->with([
-                'days.lessons.disciplineOdd',
-                'days.lessons.disciplineEven',
-                'days.lessons.firstOddCabinet',
-                'days.lessons.secondOddCabinet',
-                'days.lessons.firstEvenCabinet',
-                'days.lessons.secondEvenCabinet',
-                'days.lessons.firstOddTeacher',
-                'days.lessons.secondOddTeacher',
-                'days.lessons.firstEvenTeacher',
-                'days.lessons.secondEvenTeacher',
-                'changes.disciplineOdd',
-                'changes.disciplineEven',
-                'changes.firstOddCabinet',
-                'changes.secondOddCabinet',
-                'changes.firstEvenCabinet',
-                'changes.secondEvenCabinet',
-                'changes.firstOddTeacher',
-                'changes.secondOddTeacher',
-                'changes.firstEvenTeacher',
-                'changes.secondEvenTeacher',
+                'days.lessons.oddDiscipline:id,name',
+                'days.lessons.evenDiscipline:id,name',
+                'days.lessons.firstOddCabinet:id,name',
+                'days.lessons.secondOddCabinet:id,name',
+                'days.lessons.firstEvenCabinet:id,name',
+                'days.lessons.secondEvenCabinet:id,name',
+                'days.lessons.firstOddTeacher:id,name',
+                'days.lessons.secondOddTeacher:id,name',
+                'days.lessons.firstEvenTeacher:id,name',
+                'days.lessons.secondEvenTeacher:id,name',
+                'changes.oddDiscipline:id,name',
+                'changes.evenDiscipline:id,name',
+                'changes.firstOddCabinet:id,name',
+                'changes.secondOddCabinet:id,name',
+                'changes.firstEvenCabinet:id,name',
+                'changes.secondEvenCabinet:id,name',
+                'changes.firstOddTeacher:id,name',
+                'changes.secondOddTeacher:id,name',
+                'changes.firstEvenTeacher:id,name',
+                'changes.secondEvenTeacher:id,name',
             ])
             ->first();
         return new GroupResource($group);
@@ -82,16 +83,18 @@ class GroupController extends Controller
         return new GroupResource($group);
     }
 
-    public function update(GroupUpdateRequest $request, $slug)
+    public function update(GroupUpdateRequest $request, $slug): GroupResource
     {
         $group = Group::where('slug', $slug)->first();
         $group->update($request->all());
+        return new GroupResource($group);
     }
 
-    public function destroy($slug)
+    public function destroy($slug): JsonResponse
     {
         $group = Group::where('slug', $slug)->first();
         $group->delete();
+        return response()->json(['id' => $group->id]);
     }
 
     public function courseGroups($course): AnonymousResourceCollection
@@ -99,34 +102,34 @@ class GroupController extends Controller
         return GroupResource::collection(Group::where('course', $course)->get());
     }
 
-    public function fresh(Request $request)
+    public function fresh(Request $request): JsonResponse
     {
         $groups = Group::all();
         foreach ($groups as $group) {
+            $changes = $group->changes;
+
             $day = $group->days()->where('name', $request->day)->first();
             $lessons = $day->lessons;
-
-                $changes = $group->changes;
 
             for ($i = 0; $i < 5; ++$i) {
                 $lesson = $lessons[$i];
                 $data = [
-                    'disciplineOdd_id' => $lesson->disciplineOdd_id,
-                    'disciplineEven_id' => $lesson->disciplineEven_id,
-                    'firstOddTeacher_id' => $lesson->firstOddTeacher_id,
-                    'secondOddTeacher_id' => $lesson->secondOddTeacher_id,
-                    'firstEvenTeacher_id' => $lesson->firstEvenTeacher_id,
-                    'secondEvenTeacher_id' => $lesson->secondEvenTeacher_id,
+                    'oddDiscipline_id' => $lesson->oddDiscipline_id,
+                    'evenDiscipline_id' => $lesson->evenDiscipline_id,
 
                     'firstOddCabinet_id' => $lesson->firstOddCabinet_id,
                     'secondOddCabinet_id' => $lesson->secondOddCabinet_id,
                     'firstEvenCabinet_id' => $lesson->firstEvenCabinet_id,
                     'secondEvenCabinet_id' => $lesson->secondEvenCabinet_id,
+
+                    'firstOddTeacher_id' => $lesson->firstOddTeacher_id,
+                    'secondOddTeacher_id' => $lesson->secondOddTeacher_id,
+                    'firstEvenTeacher_id' => $lesson->firstEvenTeacher_id,
+                    'secondEvenTeacher_id' => $lesson->secondEvenTeacher_id,
                 ];
                 $changes[$i]->update($data);
             }
         }
-
         return response()->json(['message' => 'success']);
     }
 }
